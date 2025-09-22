dde9868cd892e9c055274f693ae75e1b60d4dd3e
DHARANISH-M <dharanish0631@gmail.com>
2025-09-22 12:04:14 +0530

    added paragraph

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>This is my First deployment</h1>
+      <p>Last week, I worked on the backend development of the feed page in my project. My main task was to integrate the API with the database and ensure that the data flow was functioning correctly. To verify this, I tested all the APIs using Postman. The responses were checked carefully, and I confirmed that the APIs were fetching and updating the required data accurately. This helped me make sure that the backend is stable and ready for frontend integration.</p>
       <button>Welcome</button>
+
     </div>
   );
 }
